e30d634f706594db8f25d7069561b6d5631fb314
HuongMT98 <vndong998@gmail.com>
2024-05-07 17:38:31 +0700

    Body Load more item

--- src/components/Layout/BodyWrapItem/BodyWrapItemContent/BodyWrapItemContent.js
+++ src/components/Layout/BodyWrapItem/BodyWrapItemContent/BodyWrapItemContent.js
@@ -1,8 +1,45 @@
+import axios from "axios"
+import { useEffect, useState } from "react"
+import { Link } from "react-router-dom"
+
 function BodyWrapItemContent() {
+  const [data, setData] = useState([])
+  const [visible, setVisible] = useState(12)
+  useEffect(() => {
+    axios
+      .get("http://localhost:5200/products")
+      .then((res) => setData(res.data))
+      .catch((err) => console.log(err))
+  }, [])
+
+  const handleLoadMore = () => {
+    setVisible(visible + 12)
+  }
+
   return (
     <>
-      <div className='body-wrap-item-content'>
-        <h1>Render UI</h1>
+      <div className='bodywrapitem-container'>
+        <div className='bodywrapitem'>
+          {data.slice(0, visible).map((item) => (
+            <Link key={item.id} className='bodywrapitem-item'>
+              <img src={item.image} alt='' />
+              <p className='bodywrapitem-item-name'>{item.name}</p>
+              <div className='bodywrapitem-item-infosell'>
+                <p className='bodywrapitem-item-price'>{item.price}</p>
+                <p className='bodywrapitem-item-sold'>{item.sold} sold</p>
+              </div>
+              {console.log(item)}
+            </Link>
+          ))}
+        </div>
+        <div className='bodywrapitem-loadmore'>
+          <button
+            className='bodywrapitem-loadmore-btn'
+            onClick={handleLoadMore}
+          >
+            See more
+          </button>
+        </div>
       </div>
     </>
   )
