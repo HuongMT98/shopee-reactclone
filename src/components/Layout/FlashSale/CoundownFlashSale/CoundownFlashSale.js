ea3b9303c200f5169760f1cd814abb6fe1471846
HuongMT98 <vndong998@gmail.com>
2024-05-08 16:32:29 +0700

    FlashSale component

--- src/components/Layout/FlashSale/CoundownFlashSale/CoundownFlashSale.js
+++ src/components/Layout/FlashSale/CoundownFlashSale/CoundownFlashSale.js
@@ -1,16 +1,52 @@
-// import CountdownTimer from "react-component-countdown-timer"
+import { useEffect, useState } from "react"
 
-// const CountdownFlashSale = () => {
-//   return (
-//     <div className='countdown-content'>
-//       <CountdownTimer count={7200} border />
-//     </div>
-//   )
-// }
+const CoundownFlashSale = () => {
+  const [days, setDays] = useState(0)
+  const [hours, setHours] = useState(0)
+  const [minutes, setMinutes] = useState(0)
+  const [seconds, setSeconds] = useState(0)
 
-// export default CountdownFlashSale
-function CountdownFlashSale() {
-  return <></>
+  const deadline = "December, 31, 2024"
+
+  const getTime = () => {
+    const time = Date.parse(deadline) - Date.now()
+
+    setDays(Math.floor(time / (1000 * 60 * 60 * 24)))
+    setHours(Math.floor((time / (1000 * 60 * 60)) % 24))
+    setMinutes(Math.floor((time / 1000 / 60) % 60))
+    setSeconds(Math.floor((time / 1000) % 60))
+  }
+
+  useEffect(() => {
+    const interval = setInterval(() => getTime(deadline), 1000)
+
+    return () => clearInterval(interval)
+  }, [])
+
+  return (
+    <div className='timer' role='timer'>
+      <div className='col-4'>
+        <div className='box'>
+          <p id='day'>{days < 10 ? "0" + days : days}</p>
+        </div>
+      </div>
+      <div className='col-4'>
+        <div className='box'>
+          <p id='hour'>{hours < 10 ? "0" + hours : hours}</p>
+        </div>
+      </div>
+      <div className='col-4'>
+        <div className='box'>
+          <p id='minute'>{minutes < 10 ? "0" + minutes : minutes}</p>
+        </div>
+      </div>
+      <div className='col-4'>
+        <div className='box'>
+          <p id='second'>{seconds < 10 ? "0" + seconds : seconds}</p>
+        </div>
+      </div>
+    </div>
+  )
 }
 
-export default CountdownFlashSale
+export default CoundownFlashSale
