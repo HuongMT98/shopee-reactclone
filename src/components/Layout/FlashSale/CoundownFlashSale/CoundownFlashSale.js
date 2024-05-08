4956c86eb7bdcc01cff559c85c349010a05adc68
HuongMT98 <vndong998@gmail.com>
2024-05-08 15:27:48 +0700

    CleanCode

--- src/components/Layout/FlashSale/CoundownFlashSale/CoundownFlashSale.js
+++ src/components/Layout/FlashSale/CoundownFlashSale/CoundownFlashSale.js
@@ -1,11 +1,16 @@
-import CountdownTimer from "react-component-countdown-timer"
+// import CountdownTimer from "react-component-countdown-timer"
 
-const CountdownFlashSale = () => {
-  return (
-    <div className='countdown-content'>
-      <CountdownTimer count={7200} border />
-    </div>
-  )
+// const CountdownFlashSale = () => {
+//   return (
+//     <div className='countdown-content'>
+//       <CountdownTimer count={7200} border />
+//     </div>
+//   )
+// }
+
+// export default CountdownFlashSale
+function CountdownFlashSale() {
+  return <></>
 }
 
 export default CountdownFlashSale
