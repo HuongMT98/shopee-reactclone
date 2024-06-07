ba6c60add44728c5ab022964dcab253799685c22
HuongMT98 <vndong998@gmail.com>
2024-06-07 12:35:43 +0700

    setup clean db

--- backend/src/configs/environment.js
+++ backend/src/configs/environment.js
@@ -4,6 +4,8 @@ const env = {
   MONGODB_URI: process.env.MONGODB_URI,
   DATABASE_NAME: process.env.DATABASE_NAME,
   AUTHOR: process.env.AUTHOR,
+  HOST: process.env.HOST,
+  POST: process.env.POST,
 }
 
 export default env
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,24 +1,22 @@
 /* eslint-disable no-unused-vars */
 import express from "express"
 import { CONNECT_DB, GET_DB } from "./configs/mongodb.js"
+import env from "./configs/environment.js"
 
 const START_SERVER = () => {
   const app = express()
-  const host = "http://localhost"
-  const port = 5000
 
   app.get("/", async (req, res) => {
-    res.send("Hello World!")
+    res.send("<h1>Welcome to Backend Server</h1>")
   })
 
-  app.listen(port, () => {
-    console.log(`3: Backend server đang chạy tại: ${host}:${port}`)
+  app.listen(env.POST, () => {
+    console.log(`3: Backend server đang chạy tại: ${env.HOST}:${env.POST}`)
   })
 }
 
 //Chỉ khi kết nối database thành công thì mới start server
 //Cách 1: Dùng anonymous Async function
-
 ;(async () => {
   try {
     console.log("1: Đang kết nối Database MongoDB...")
