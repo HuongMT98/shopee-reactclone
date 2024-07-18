a67b5526fe32443a197882e497c5d104e3169032
HuongMT98 <vndong998@gmail.com>
2024-07-18 14:01:23 +0700

    Reclean Code

--- backend/src/routes/v1/index.js
+++
@@ -1,9 +0,0 @@
-import express from "express"
-import { productRoutes } from "./productRouter.js"
-
-const Router = express.Router()
-
-//API Product
-Router.use("/product", productRoutes)
-
-export default Router
--- src/components/Layout/NotFound/NotFound.js
+++ src/components/Layout/NotFound/NotFound.js
@@ -8,9 +8,15 @@ function NotFound() {
       <Nav />
       <div className='not-found'>
         <div className='not-found-container'>
-          <h1>404 Page Not Found</h1>
-          <h3>
-            Contact Administrator or <NavLink to='/'>Return Home</NavLink>
+          <h1 className='not-found-title'>404</h1>
+          <h2 className='not-found-subtitle'>
+            Page <span>Not Found</span>
+          </h2>
+          <h3 className='not-found-description'>
+            Contact Administrator or{" "}
+            <NavLink to='/' className={"not-found-link"}>
+              Return Home
+            </NavLink>
           </h3>
         </div>
       </div>
