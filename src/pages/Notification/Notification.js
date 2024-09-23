ec80684efabeed87383212fbd11ffbc884efeb29
HuongMT98 <vndong998@gmail.com>
2024-09-23 17:08:43 +0700

    Fix notification

--- src/pages/Notification/Notification.js
+++ src/pages/Notification/Notification.js
@@ -1,12 +1,28 @@
 import Footer from "../../components/Layout/Footer/Footer"
 import Nav from "../../components/Layout/Nav/Nav"
 import "./Notification.scss"
+import { useEffect, useState } from "react"
+import ApiNotification from "../../Api/ApiNotification"
+
 function Notification() {
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    ApiNotification().then((data) => {
+      setData(data)
+    })
+  }, [])
   return (
     <>
       <Nav />
-      <div className='notification-container'>
-        <h1>Notification Page</h1>
+      <div className='notification'>
+        <div className='notification-container'>
+          {data.map((item) => (
+            <div className='notification-item' key={item.id}>
+              <p>{item.content}</p>
+            </div>
+          ))}
+        </div>
       </div>
       <Footer />
     </>
